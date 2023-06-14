8b20ed7a2aa7b13c94f4aa3e5984ccfb93be174e
danmera1980 <danmera1980@gmail.com>
2023-06-14 11:07:34 -0500

    Fixed the routing problem

--- client/src/app/modules/auth/auth.module.ts
+++ client/src/app/modules/auth/auth.module.ts
@@ -3,17 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SharedModule } from '@app/shared/shared.module';
-
-
+import { AuthRoutingModule } from './auth.routing';
 
 @NgModule({
-  declarations: [
-    RegisterComponent,
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    SharedModule
-  ]
+  declarations: [RegisterComponent, LoginComponent],
+  imports: [CommonModule, SharedModule, AuthRoutingModule],
 })
-export class AuthModule { }
+export class AuthModule {}
--- client/src/app/modules/auth/auth.resolver.ts
+++
@@ -1,5 +0,0 @@
-import { ResolveFn } from '@angular/router';
-
-export const authResolver: ResolveFn<boolean> = (route, state) => {
-  return true;
-};
